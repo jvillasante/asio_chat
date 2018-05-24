1dc7bdb919fc83c20d6804890003aeaf139ee58d
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2018-05-24 11:27:25 -0400

    Adds recent messages

--- src/asio_chat_example/chat_server.cpp
+++ src/asio_chat_example/chat_server.cpp
@@ -32,10 +32,22 @@ typedef std::shared_ptr<chat_participant> chat_participant_ptr;
 
 class chat_room {
 public:
-  void join(chat_participant_ptr participant) { participants_.insert(participant); }
+  void join(chat_participant_ptr participant) {
+    participants_.insert(participant);
+
+    for (const auto& msg : recent_msgs_) {
+      participant->deliver(msg);
+    }
+  }
+
   void leave(chat_participant_ptr participant) { participants_.erase(participant); }
 
   void deliver(const chat_message& msg) {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs) {
+      recent_msgs_.pop_front();
+    }
+
     for (auto participant : participants_) {
       if (std::strcmp(msg.from(), participant->to()) == 0) {
         if (std::strcmp(msg.to(), participant->from()) == 0) {
@@ -47,6 +59,8 @@ public:
 
 private:
   std::set<chat_participant_ptr> participants_;
+  enum { max_recent_msgs = 100 };
+  chat_message_queue recent_msgs_;
 };
 
 //----------------------------------------------------------------------
